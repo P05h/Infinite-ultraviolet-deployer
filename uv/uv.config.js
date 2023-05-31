4a66f8dacaf3484f1a263c4d64b83c2349797400
P05h <121305788+P05h@users.noreply.github.com>
2023-05-31 18:45:08 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://green-breeze-1701.rambubanrambutan2728.workers.dev/',
+    bare: 'https://tiw.tiwweb.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
